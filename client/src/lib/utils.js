1cd2be0e221998bd119d893a85cef45b7311299c
Tomoe-12 <li8993han@gmail.com>
2024-10-10 19:58:56 +0630

    profile ui

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,3 +4,31 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+// export const colors = [
+//   "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#f006faa]",
+//   "bg-[#ffd60a2a] text-[#ffd60a] border-[1px] border-[#ffd60abb]",
+//   "bg-[#06d6a02a] text-[#06d6a0] border-[1px] border-[#06d6a0bb]",
+//   "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]",
+// ];
+
+// export const getColor = (color) => {
+//   if (color >= 0 && color < colors.length) {
+//     return colors[color];
+//   }
+//   return colors[0]; // Fallback to the first color if out of range
+// };
+
+export const colors = [
+  "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]", // Soft Blue: For message bubbles
+  "bg-[#06d6a02a] text-[#06d6a0] border-[1px] border-[#06d6a0bb]", // Vibrant Green: For status indicators or success messages
+  "bg-[#ffd60a2a] text-[#ffd60a] border-[1px] border-[#ffd60abb]", // Bright Yellow: For notifications or highlights
+  "bg-[#ff006e2a] text-[#ff006e] border-[1px] border-[#ff006ebb]", // Bold Pink: For error messages or urgent alerts
+];
+
+export const getColor = (color) => {
+  if (color >= 0 && color < colors.length) {
+    return colors[color];
+  }
+  return colors[0]; // Fallback to the first color if out of range
+};
--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,14 +1,123 @@
-import { useAppStore } from '@/store'
-import React from 'react'
+import { useAppStore } from "@/store";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { colors, getColor } from "@/lib/utils";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
-  const { userInfo } = useAppStore()
-  return (
-    <div>
-      Profile
-      <div>Email : {userInfo.email}</div>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
 
-export default Profile
+  const saveChanges = async () => {};
+
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max ">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer " />
+        </div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover h-full bg-black"
+                />
+              ) : (
+                <div
+                  className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(
+                    selectedColor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.split("").shift()
+                    : userInfo.email.split("").shift()}
+                </div>
+              )}
+            </Avatar>
+            {hovered && (
+              <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 cursor-pointer rounded-full">
+                {image ? (
+                  <FaTrash className="text-white text-3xl cursor-pointer" />
+                ) : (
+                  <FaPlus className="text-white text-3xl cursor-pointer" />
+                )}
+              </div>
+            )}
+            {/* input type='text' */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 text-white gap-5 flex-col items-center justify-center">
+            <div className="w-full">
+              <Input
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Fist Name"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full">
+              <Input
+                placeholder="Second Name"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className="w-full flex gap-5 ">
+              {colors.map((color, index) => (
+                <div
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-200
+                  ${
+                    selectedColor === index
+                      ? "outline outline-whtie/50 outline-1"
+                      : ""
+                  }}
+                    `}
+                  key={index}
+                  onClick={() => setSelectedColor(index)}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <Button
+            className={`h-16 w-full bg-primaryColor hover:bg-purple-900 transition-all duration-200  ${getColor(selectedColor)}`}
+            onClick={saveChanges}
+          >
+            Save Changes
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
