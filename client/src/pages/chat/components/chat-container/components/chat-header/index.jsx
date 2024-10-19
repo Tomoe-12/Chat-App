c5e08aa01597c4857fd997eaa5c6b042d32c8642
Tomoe-12 <li8993han@gmail.com>
2024-10-19 10:47:32 +0630

    message ui done

--- client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
+++ client/src/pages/chat/components/chat-container/components/chat-header/index.jsx
@@ -6,7 +6,7 @@ import { RiCloseFill } from "react-icons/ri";
 const ChatHeader = () => {
   const { closeChat, selectedChatData, selectedChatType } = useAppStore();
   return (
-    <div className="h-[10vw] border-b-2 border-[#2f303b] flex items-center justify-between px-20  ">
+    <div className="h-[10vh] border-b-2 border-[#2f303b] flex items-center justify-between px-20  ">
       <div className="flex gap-5 items-center justify-between w-full  ">
         <div className="flex gap-3 items-center justify-center ">
           <div className="w-12 h-12 relative">
--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,7 +1,65 @@
+import { useAppStore } from "@/store";
+import moment from "moment";
+import { useEffect } from "react";
+import { useRef } from "react";
+
 const MessageContainer = () => {
+  const scrollRef = useRef();
+  const { selectedChatData, selectedChatType, userInfo, selectedChatMessage } =
+    useAppStore();
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    }
+  }, [selectedChatMessage]);
+
+  const renderMessages = () => {
+    let lastDate = null;
+    return selectedChatMessage.map((message, i) => {
+      const messageDate = moment(message.timestamp).format("DD-MM-YYYY");
+      const showDate = messageDate !== lastDate;
+      lastDate = messageDate;
+      return (
+        <div key={i}>
+          {showDate && (
+            <div className="text-center my-2 text-gray-500">
+              {moment(message.timestamp).format("LL")}
+            </div>
+          )}
+          {selectedChatType === "contact" && renderedMessages(message)}
+        </div>
+      );
+    });
+  };
+
+  const renderedMessages = (message) => (
+    <div
+      className={` ${
+        message.sender === selectedChatData._id ? "text-left" : "text-right"
+      } `}
+    >
+      {message.messageType === "text" && (
+        <div
+          className={` ${
+            message.sender !== selectedChatData._id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50  "
+              : " bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"
+          } border inline-block p-4 rounded my-1 max-w-[50%] break-words `}
+        >
+          {message.content}
+        </div>
+      )}
+      <div className=" text-xs text-gray-600  ">
+        {moment(message.timestamp).format("LT")}
+      </div>
+    </div>
+  );
+
   return (
     <div className="flex-1 overflow-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full ">
-      
+      {renderMessages()}
+      <div ref={scrollRef} />
     </div>
   );
 };
