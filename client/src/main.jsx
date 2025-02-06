d974c39b91e81ae27e714fb63ef13b46f18668d5
Tomoe-12 <li8993han@gmail.com>
2025-02-06 21:07:18 +0630

    add speedinsight

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { Toaster } from './components/ui/sonner.jsx'
 import { SocketProvider } from './context/SocketContext.jsx'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react'; // Import Speed Insights
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <App />
     <Toaster closeButton/>
     <Analytics />
+    <SpeedInsights/>
   </SocketProvider>
   // </StrictMode>,
 )
